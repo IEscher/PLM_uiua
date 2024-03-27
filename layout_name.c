65350dadb705cb4dc362a46d51868d6ec004d710
IEscher <ian.escher@gmail.com>
2024-03-27 16:17:25 +0100

    simple drawing using map

---
+++ layout_name.c
@@ -0,0 +1,84 @@
+/*******************************************************************************************
+*
+*   LayoutName v1.0.0 - Tool Description
+*
+*   LICENSE: Propietary License
+*
+*   Copyright (c) 2022 raylib technologies. All Rights Reserved.
+*
+*   Unauthorized copying of this file, via any medium is strictly prohibited
+*   This project is proprietary and confidential unless the owner allows
+*   usage in any other form by expresely written permission.
+*
+**********************************************************************************************/
+
+#include "raylib.h"
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+//----------------------------------------------------------------------------------
+// Controls Functions Declaration
+//----------------------------------------------------------------------------------
+
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main()
+{
+    // Initialization
+    //---------------------------------------------------------------------------------------
+    int screenWidth = 800;
+    int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "layout_name");
+
+    // layout_name: controls initialization
+    //----------------------------------------------------------------------------------
+    bool WindowBox000Active = true;
+    Color ColorPicker001Value = { 0, 0, 0, 0 };
+    //----------------------------------------------------------------------------------
+
+    SetTargetFPS(60);
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Implement required update logic
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR))); 
+
+            // raygui: controls drawing
+            //----------------------------------------------------------------------------------
+            if (WindowBox000Active)
+            {
+                WindowBox000Active = !GuiWindowBox((Rectangle){ 0, 40, 240, 408 }, "Tools");
+                GuiColorPicker((Rectangle){ 48, 144, 96, 96 }, NULL, &ColorPicker001Value);
+            }
+            //----------------------------------------------------------------------------------
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
+//------------------------------------------------------------------------------------
+// Controls Functions Definitions (local)
+//------------------------------------------------------------------------------------
+
